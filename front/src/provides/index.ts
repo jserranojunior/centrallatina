89cd52af73798a84c126ba165a5208f06d1f24ed
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2021-11-10 11:06:22 -0300

    remove api

--- front/src/provides/index.ts
+++ front/src/provides/index.ts
@@ -6,17 +6,17 @@ import { useUsers } from "@/modules/admin/user/use/useUsers";
 
 import { useInstitucional } from "@/modules/clientes/institucional/use/useInstitucional";
 // import { useFinancial } from "@/modules/financeiro/use/useFinancial";
-import useApiConnect from "@/modules/clientes/api/use/useApiConnect";
+// import useApiConnect from "@/modules/clientes/api/use/useApiConnect";
 import { useAuthAdm } from "@/modules/admin/authadm/use/useAuthAdm";
 
-const ApiConnect = new useApiConnect();
+// const ApiConnect = new useApiConnect();
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const Provides = () => {
   // provide("financial", useFinancial());
   provide("auth", useAuth());
   provide("authadm", useAuthAdm());
 
-  provide("apiconnect", ApiConnect);
+  // provide("apiconnect", ApiConnect);
   provide("institucional", useInstitucional());
   provide("users", useUsers());
   provide("resetpassword", useResetPassword());
